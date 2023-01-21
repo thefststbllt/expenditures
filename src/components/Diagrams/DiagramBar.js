ed1348be8c262188d886b24baf53436a6214fefc
thefststbllt <bel12pasha@yandex.ru>
2023-01-21 22:37:18 +0300

    fix: mobile screen crash fix

--- src/components/Diagrams/DiagramBar.js
+++ src/components/Diagrams/DiagramBar.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import './DiagramBar.css';
 
 const DiagramBar = (props) => {
@@ -7,11 +8,27 @@ const DiagramBar = (props) => {
         barFilled = Math.round(props.value / props.maxValue * 100) + '%';
     }
 
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    let barStyle = {};
+    if (width > 767) {
+        barStyle = {height: barFilled};
+    } else if (width <= 767) {
+        barStyle = {width: barFilled};
+    }
+
     return (
         <div className='diagram-bar'>
             <div className='diagram-bar__inner'>
-                <div className='diagram-bar__fill'
-                     style={{height: barFilled}}>
+                <div className='diagram-bar__fill' style={barStyle}>
                 </div>
             </div>
             <div className='diagram-bar__label'>{props.label}</div>
